e0c08e1faab76f7b69aaa37294d67d62a89a10fd
Omar <gomar@dmacc.edu>
2019-10-09 18:44:10 -0500

    Test passed

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -5,7 +5,7 @@ import fun_with_collections.sort_and_search_array as basic_list_exception
 
 class MyTestCase(unittest.TestCase):
     def test_search_array(self):
-        self.assertFalse(basic_list_exception.search_array([5, -1, 3, 6, 8, 9, 10, 4], -1))
+        self.assertFalse(basic_list_exception.search_array([5, -1, 6, 3, 8, 9, 4, 10], -1))
 
 
 def test_sort_array(self):
@@ -15,7 +15,7 @@ def test_sort_array(self):
     to_list = r.array(to_list)
 
     self.assertTrue(basic_list_exception.sort_array(([9.0, 3.0, 1.0, 5.0, 8.0, 6.0, 4.0, 7.0])))
-    """" I tried my bet, but I could not figur out the sort test"""
+    """" I tried my bet, but I could not figure out the sort test"""
 
 
 if __name__ == '__main__':
